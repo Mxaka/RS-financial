bff519b9a6fd8ce600687f86ab94142e29952fce
unsigjhb025 <unsigjhb025@student.wethinkcode.co.za>
2026-09-05 22:08:00 +0200

    created a dashboard for our existing clients

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ClientDashBoard from './Client/ClientDashBoard.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClientDashBoard />
+  </React.StrictMode>
+);
